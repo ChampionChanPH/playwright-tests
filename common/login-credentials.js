deeecb3789a6f84818762ba406ee39d8cd8fce04
Chan <christian.anasco@gmail.com>
2022-02-25 18:02:08 +0800

    remove credentials on gitignore

---
+++ common/login-credentials.js
@@ -0,0 +1,21 @@
+const SH_EMAIL = "testing.with.prosple@gmail.com"
+const SH_PASSWORD = "Prosple0000"
+const SH_FB_PASSWORD = "Prosple0000"
+const SH_EMAIL_SIGNUP = "christian.anasco+1@prosple.com"
+const SH_PASSWORD_SIGNUP = "Prosple0000"
+const EH_EMAIL = "testing.with.prosple@gmail.com"
+const EH_PASSWORD = "Prosple0000"
+const FIRST_NAME = "Christian Paul"
+const LAST_NAME = "Anasco"
+
+module.exports = {
+    SH_EMAIL,
+    SH_PASSWORD,
+    SH_FB_PASSWORD,
+    SH_EMAIL_SIGNUP,
+    SH_PASSWORD_SIGNUP,
+    EH_EMAIL,
+    EH_PASSWORD,
+    FIRST_NAME,
+    LAST_NAME
+}
