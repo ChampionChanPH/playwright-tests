67039e1d7044e7424a748dcbdede9de162032abe
Chan <christian.anasco@gmail.com>
2022-03-18 12:01:40 +0800

    added tests for seo landing pages

--- tests/student-hub/seo-landing-pages.spec.js
+++ tests/student-hub/seo-landing-pages.spec.js
@@ -39,34 +39,178 @@ test.describe('seo landing page tests', async () => {
         })
     })
 
-    // TODO: clicking the advice article title will redirect to the correct detail page
+    // choose a job on the search jobs page with "Read reviews", click the link and see that it redirects to the correct detail page
+    test("click review link to detail page", async ({ page }) => {
+        const reviews = page.locator("//div[contains(@class, 'viewport--viewport-read-reviews')]")
+        const countReviews = await reviews.count()
+        let random = getRandomNumber(1, countReviews)
+        const employerListPage = await reviews.nth(random - 1).locator("//preceding-sibling::*[contains(@class, 'Logostyle__Logo')]//p").innerText()
+        await Promise.all([
+            page.waitForNavigation(),
+            reviews.locator("//a").nth(random - 1).click()
+        ])
+        const employerOverviewPage = await page.locator("div.masthead__title h2.heading").innerText()
+        const employerReviewTitle = await page.locator("h1.heading").innerText()
+        expect(employerOverviewPage).toEqual(employerListPage)
+        expect(employerReviewTitle).toEqual(`${employerListPage} Reviews`)
+    })
+
+    // choose a job on the search jobs page, click the job title and see that it redirects to the correct detail page
+    test("click job title to detail page", async ({ page }) => {
+        const jobs = page.locator("//div[contains(@class, 'OpportunityTeaserstyle__OpportunityListing')]")
+        const countJobs = await jobs.count()
+        let random = getRandomNumber(1, countJobs)
+        const employerListPage = await jobs.nth(random - 1).locator("div.logo__item p").innerText()
+        const jobTitleListPage = await jobs.nth(random - 1).locator("div.teaser__item--title a").innerText()
+        await Promise.all([
+            page.waitForNavigation(),
+            jobs.nth(random - 1).locator("div.teaser__item--title a").click()
+        ])
+        const employerOverviewPage = await page.locator("div.masthead__title h2.heading").innerText()
+        const jobTitleOverviewPage = await page.locator("h1.heading").innerText()
+        expect(employerOverviewPage).toEqual(employerListPage)
+        expect(jobTitleOverviewPage).toEqual(jobTitleListPage)
+    })
+
+    // confirm that the apply now button on the seo landing page is working as expected
+    // clicking apply now button opens a new tab that's why it checks for 2 pages
+    test("apply now button is clickable", async ({ page, context }) => {
+        const apply = page.locator("a.button--type-apply")
+        const countApply = await apply.count()
+        let random = getRandomNumber(1, countApply)
+        // const url = await apply.nth(random - 1).getAttribute("href")
+        await Promise.all([
+            page.waitForTimeout(3000),
+            apply.nth(random - 1).click()
+        ])
+        // const urls = []
+        const pages = context.pages().length
+        // pages.forEach(element => urls.push(element._mainFrame._url))
+        expect(pages).toEqual(2)
+    })
+
+    // clicking the advice article title will redirect to the correct detail page
     test("click article title to detail page", async ({ page }) => {
-
+        const advices = page.locator("h2.heading:has-text('Advice for ')").locator("//following-sibling::div//div[@data-testid='collection-item']")
+        const adviceCount = await advices.count()
+        let random = getRandomNumber(1, adviceCount)
+        const adviceTitle = await advices.nth(random - 1).locator("h3.heading a").innerText()
+        console.log("chosen article:", adviceTitle)
+        await Promise.all([
+            page.waitForNavigation(),
+            advices.nth(random - 1).locator("h3.heading a").click()
+        ])
+        const adviceTitleDetailPage = await page.locator("h1.heading").innerText()
+        console.log("detail page article:", adviceTitleDetailPage)
+        expect(adviceTitleDetailPage).toEqual(adviceTitle)
     })
 
-    // TODO: clicking the video title will redirect to the correct detail page
+    // clicking the video title will redirect to the correct detail page
     test("click video title to detail page", async ({ page }) => {
-
+        const videos = page.locator("h2.heading:has-text('Videos for ')").locator("//following-sibling::div//div[@data-testid='collection-item']")
+        const videoCount = await videos.count()
+        let random = getRandomNumber(1, videoCount)
+        const videoTitle = await videos.nth(random - 1).locator("h4.heading a").innerText()
+        console.log("chosen video:", videoTitle)
+        await Promise.all([
+            page.waitForNavigation(),
+            videos.nth(random - 1).locator("h4.heading a").click()
+        ])
+        const videoTitleDetailPage = await page.locator("h1.heading").innerText()
+        console.log("detail page video:", videoTitleDetailPage)
+        expect(videoTitleDetailPage).toEqual(videoTitle)
     })
 
-    // TODO: clicking the "Read full story" button on graduate stories section will redirect to the correct detail page
+    // clicking the video thumbnail will redirect to the correct detail page
+    test("click video thumbnail to detail page", async ({ page }) => {
+        const videos = page.locator("h2.heading:has-text('Videos for ')").locator("//following-sibling::div//div[@data-testid='collection-item']")
+        const videoCount = await videos.count()
+        let random = getRandomNumber(1, videoCount)
+        const videoTitle = await videos.nth(random - 1).locator("h4.heading a").innerText()
+        console.log("chosen video:", videoTitle)
+        await Promise.all([
+            page.waitForNavigation(),
+            videos.nth(random - 1).locator("//div[contains(@class, 'VideoSnippetstyle__VideoSnippet')]/a").click()
+        ])
+        const videoTitleDetailPage = await page.locator("h1.heading").innerText()
+        console.log("detail page video:", videoTitleDetailPage)
+        expect(videoTitleDetailPage).toEqual(videoTitle)
+    })
+
+    // clicking the "Read full story" button on graduate stories section will redirect to the correct detail page
     test("click read full story button in graduate stories to detail page", async ({ page }) => {
-
+        const stories = page.locator("h2.heading:has-text(' Graduate Success Stories')").locator("//following-sibling::div//div[@data-testid='collection-item']")
+        const storyCount = await stories.count()
+        let random = getRandomNumber(1, storyCount)
+        const name = await stories.nth(random - 1).locator("div.viewport--large h3.heading a").innerText()
+        const jobTitle = await stories.nth(random - 1).locator("//div[contains(@class, 'viewport--large')]/div[contains(@class, 'StorySnippetstyle__Byline')]").innerText()
+        console.log(`chosen story: ${name} (${jobTitle})`)
+        await Promise.all([
+            page.waitForNavigation(),
+            stories.nth(random - 1).locator("//div[contains(@class, 'StorySnippetstyle__StorySnippetActions')]/a").click()
+        ])
+        const nameDetailPage = await page.locator("h1.heading").innerText()
+        const jobTitleDetailPage = await page.locator("//div[contains(@class, 'StoryDetailstyle__Byline')]").innerText()
+        console.log(`detail page story: ${nameDetailPage} (${jobTitleDetailPage})`)
+        expect.soft(nameDetailPage).toEqual(name)
+        expect(jobTitleDetailPage).toEqual(jobTitle)
     })
 
-    // TODO: clicking the name of the person featured in graduate stories section will redirect to the correct detail page
+    // clicking the name of the person featured in graduate stories section will redirect to the correct detail page
     test("click name in graduate stories to detail page", async ({ page }) => {
-
+        const stories = page.locator("h2.heading:has-text(' Graduate Success Stories')").locator("//following-sibling::div//div[@data-testid='collection-item']")
+        const storyCount = await stories.count()
+        let random = getRandomNumber(1, storyCount)
+        const name = await stories.nth(random - 1).locator("div.viewport--large h3.heading a").innerText()
+        const jobTitle = await stories.nth(random - 1).locator("//div[contains(@class, 'viewport--large')]/div[contains(@class, 'StorySnippetstyle__Byline')]").innerText()
+        console.log(`chosen story: ${name} (${jobTitle})`)
+        await Promise.all([
+            page.waitForNavigation(),
+            stories.nth(random - 1).locator("div.viewport--large h3.heading a").click()
+        ])
+        const nameDetailPage = await page.locator("h1.heading").innerText()
+        const jobTitleDetailPage = await page.locator("//div[contains(@class, 'StoryDetailstyle__Byline')]").innerText()
+        console.log(`detail page story: ${nameDetailPage} (${jobTitleDetailPage})`)
+        expect.soft(nameDetailPage).toEqual(name)
+        expect(jobTitleDetailPage).toEqual(jobTitle)
     })
 
-    // TODO: clicking the "Read full story" button on day in the life stories section will redirect to the correct detail page
+    // clicking the "Read full story" button on day in the life stories section will redirect to the correct detail page
     test("click read full story button in day in the life to detail page", async ({ page }) => {
-
+        const stories = page.locator("h2.heading:has-text(' Day in the Life Stories')").locator("//following-sibling::div//div[@data-testid='collection-item']")
+        const storyCount = await stories.count()
+        let random = getRandomNumber(1, storyCount)
+        const name = await stories.nth(random - 1).locator("div.viewport--large h3.heading a").innerText()
+        const jobTitle = await stories.nth(random - 1).locator("//div[contains(@class, 'viewport--large')]/div[contains(@class, 'StorySnippetstyle__Byline')]").innerText()
+        console.log(`chosen story: ${name} (${jobTitle})`)
+        await Promise.all([
+            page.waitForNavigation(),
+            stories.nth(random - 1).locator("//div[contains(@class, 'StorySnippetstyle__StorySnippetActions')]/a").click()
+        ])
+        const nameDetailPage = await page.locator("h1.heading").innerText()
+        const jobTitleDetailPage = await page.locator("//div[contains(@class, 'StoryDetailstyle__Byline')]").innerText()
+        console.log(`detail page story: ${nameDetailPage} (${jobTitleDetailPage})`)
+        expect.soft(nameDetailPage).toEqual(name)
+        expect(jobTitleDetailPage).toEqual(jobTitle)
     })
 
-    // TODO: clicking the name of the person featured in day in the life stories section will redirect to the correct detail page
+    // clicking the name of the person featured in day in the life stories section will redirect to the correct detail page
     test("click name in day in the life to detail page", async ({ page }) => {
-
+        const stories = page.locator("h2.heading:has-text(' Day in the Life Stories')").locator("//following-sibling::div//div[@data-testid='collection-item']")
+        const storyCount = await stories.count()
+        let random = getRandomNumber(1, storyCount)
+        const name = await stories.nth(random - 1).locator("div.viewport--large h3.heading a").innerText()
+        const jobTitle = await stories.nth(random - 1).locator("//div[contains(@class, 'viewport--large')]/div[contains(@class, 'StorySnippetstyle__Byline')]").innerText()
+        console.log(`chosen story: ${name} (${jobTitle})`)
+        await Promise.all([
+            page.waitForNavigation(),
+            stories.nth(random - 1).locator("div.viewport--large h3.heading a").click()
+        ])
+        const nameDetailPage = await page.locator("h1.heading").innerText()
+        const jobTitleDetailPage = await page.locator("//div[contains(@class, 'StoryDetailstyle__Byline')]").innerText()
+        console.log(`detail page story: ${nameDetailPage} (${jobTitleDetailPage})`)
+        expect.soft(nameDetailPage).toEqual(name)
+        expect(jobTitleDetailPage).toEqual(jobTitle)
     })
 
     // TODO: clicking the employer name will redirect to the correct detail page
