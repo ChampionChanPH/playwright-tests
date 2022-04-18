704f2d16b4bf0ab8fa9adbb89bff5f61e3b535ec
Chan <christian.anasco@gmail.com>
2022-04-18 22:00:47 +0800

    update

--- tests/employer-hub/advice.spec.js
+++ tests/employer-hub/advice.spec.js
@@ -3,6 +3,9 @@ const { getRandomNumber, getRandomCharacters } = require("../../common/common-fu
 const { CompleteLogin, Input } = require("../../common/common-classes")
 const data = require("../../common/common-details.json")
 
+const csv = "./resources/blank.xlsx"
+const prospleLogo = "./resources/prosple-logo.png"
+
 test.beforeEach(async ({ page }) => {
     const login = new CompleteLogin(page)
     await login.employerHubLogin()
@@ -54,7 +57,7 @@ test.describe('edit advice article', async () => {
 
         // test to change the article type
         // check for error message when selected ---
-        test.only('update article type', async ({ page }) => {
+        test('update article type', async ({ page }) => {
             const input = new Input(page)
             await page.locator("select[name=type]").selectOption({ label: "---" })
             await page.click("button.button span:has-text('Save')")
@@ -63,6 +66,166 @@ test.describe('edit advice article', async () => {
             await input.randomSelect("select[name=type]", false)
             await page.click("button.button span:has-text('Save')")
             await page.locator("//a[text()='Close']").click()
+        })
+
+        // test for the industry sector section
+        test('update industry sector', async ({ page }) => {
+            await page.locator("//div[contains(@class, 'CheckboxTree__Container')]").nth(0).waitFor()
+            const label = page.locator("//span[label/text()='Industry Sector']/following-sibling::div")
+            const industrySectors = label.locator("//div[contains(@class, 'CheckboxGroup__CheckboxContainer-sc')]")
+            const countSectors = await industrySectors.count()
+            for (let i = 0; i < countSectors; i++) {
+                const isChecked = await industrySectors.nth(i).locator("input").getAttribute("class")
+                if (isChecked == "is-checked") {
+                    await industrySectors.nth(i).locator("label").click()
+                    const getClass = await industrySectors.nth(i).locator("input").getAttribute("class")
+                    expect(getClass).not.toEqual("is-checked")
+                }
+            }
+            for (let i = 0; i < countSectors; i++) {
+                const random = getRandomNumber(1, 2)
+                if (random == 1) {
+                    await industrySectors.nth(i).locator("label").click()
+                    const getClass = await industrySectors.nth(i).locator("input").getAttribute("class")
+                    expect(getClass).toEqual("is-checked")
+                }
+            }
+            await page.click("button.button span:has-text('Save')")
+            await page.locator("//a[text()='Close']").click()
+        })
+
+        // test for the study field section
+        test('update study field', async ({ page }) => {
+            await page.locator("//div[contains(@class, 'CheckboxTree__Container')]").nth(1).waitFor()
+            const label = page.locator("//span[label/text()='Study Field']/following-sibling::div")
+            const industrySectors = label.locator("//div[contains(@class, 'CheckboxGroup__CheckboxContainer-sc')]")
+            const countSectors = await industrySectors.count()
+            for (let i = 0; i < countSectors; i++) {
+                const isChecked = await industrySectors.nth(i).locator("input").getAttribute("class")
+                if (isChecked == "is-checked") {
+                    await industrySectors.nth(i).locator("label").click()
+                    const getClass = await industrySectors.nth(i).locator("input").getAttribute("class")
+                    expect(getClass).not.toEqual("is-checked")
+                }
+            }
+            for (let i = 0; i < countSectors; i++) {
+                const random = getRandomNumber(1, 2)
+                if (random == 1) {
+                    await industrySectors.nth(i).locator("label").click()
+                    const getClass = await industrySectors.nth(i).locator("input").getAttribute("class")
+                    expect(getClass).toEqual("is-checked")
+                }
+            }
+            await page.click("button.button span:has-text('Save')")
+            await page.locator("//a[text()='Close']").click()
+        })
+
+        test('update locations', async ({ page }) => {
+            const input = new Input(page)
+            const label = page.locator("//span[label/text()='Locations']/following-sibling::div")
+            await label.locator("//button[text()='Remove']").waitFor()
+            const remove = label.locator("//button[text()='Remove']")
+            const removeCount = await remove.count()
+            for (let i = 0; i < removeCount; i++) {
+                await remove.nth(0).click()
+            }
+            await label.locator("//button[text()='Add']").click()
+            await input.randomSelect("//span[label/text()='Locations']/following-sibling::div//select[contains(@class, 'sc-khIgXV')]", false)
+            await page.click("button.button span:has-text('Save')")
+            await page.locator("//a[text()='Close']").click()
+        })
+
+        // test that clicking the next button goes to the next page
+        test('next button is working', async ({ page }) => {
+            const menu = await page.locator("//span[contains(@class, 'Stepperstyle__StepLabel-sc')]").allInnerTexts()
+            const current = await page.locator("span.eOSPvU").innerText()
+            const currentIndex = menu.indexOf(current)
+            console.log(`Current menu: ${current}, index: ${currentIndex}`)
+            await page.locator("button.button span:has-text('Next')").click()
+            const active = await page.locator("span.eOSPvU").innerText()
+            const activeIndex = menu.indexOf(active)
+            console.log(`Current menu: ${active}, index: ${activeIndex}`)
+            expect(activeIndex).toBeGreaterThan(currentIndex)
+        })
+    })
+
+    // tests for the content section on job opportunities
+    test.describe('content section', async () => {
+        // before each test, go to the content section
+        test.beforeEach(async ({ page }) => {
+            await page.locator("//span[contains(@class, 'Stepperstyle__StepLabel-sc') and text()='Content']").click()
+        })
+
+        // test to update the introduction
+        test('update the introduction field', async ({ page }) => {
+            const intro = getRandomCharacters(6)
+            const intro_content = "This is the article introduction with some random characters:"
+            await page.locator("textarea[name=introduction]").fill(`${intro_content} ${intro}.`)
+            await page.click("button.button span:has-text('Save')")
+            await Promise.all([
+                page.waitForNavigation(),
+                page.locator("//button[text()='Preview']").click()
+            ])
+            const introPreview = await page.locator("//p[contains(@class, 'PageHeadingstyle__Subheading')]").innerText()
+            expect(introPreview).toEqual(`${intro_content} ${intro}.`)
+        })
+
+        // test to update the featured image section
+        // check for error message when added incorrect file type
+        test('update the featured image section', async ({ page }) => {
+            const image = page.locator("//span[label/text()='Featured Image']/following-sibling::div")
+            await image.locator("span.icon--pencil").click()
+            await image.locator("input[name=featuredImage]").setInputFiles(csv)
+            await expect(page.locator("span.icon--danger")).toBeVisible()
+            await expect(page.locator("//p[contains(@class, 'Formstyle__ErrorMessage') and text()='Unsupported format']")).toBeVisible()
+            await image.locator("input[name=featuredImage]").setInputFiles(prospleLogo)
+            await page.click("button.button span:has-text('Save')")
+            await page.locator("//a[text()='Close']").click()
+        })
+
+        // test to update the summary field
+        test('update the summary field', async ({ page }) => {
+            const summary = getRandomCharacters(6)
+            const summary_content = "This is the article summary with some random characters:"
+            await page.locator("textarea[name=summary]").fill(`${summary_content} ${summary}.`)
+            await page.click("button.button span:has-text('Save')")
+            await page.locator("//a[text()='Close']").click()
+            await Promise.all([
+                page.waitForNavigation(),
+                page.locator("//a[contains(@class, 'Navigationstyle__MenuLink') and span/text()='Advice']").nth(1).click()
+            ])
+            await page.locator("//div[contains(@class, 'Content__ContentBox-sc')]//a[text()='Edit']").nth(0).waitFor()
+            const summaryListPage = await page.locator("//p[contains(@class, 'GenericTeaserstyle__Description-sc')]").nth(0).innerText()
+            expect(summaryListPage).toEqual(`${summary_content} ${summary}.`)
+        })
+
+        // test to update the body field
+        // check for error message when field was left blank
+        test('update the body field', async ({ page }) => {
+            const body = getRandomCharacters(6)
+            const body_content = "This is the body of an advice article with some random characters:"
+            await page.locator("div.ck-editor__editable").click()
+            await page.keyboard.press("Control+A")
+            await page.keyboard.press("Delete")
+            await page.click("button.button span:has-text('Save')")
+            await expect(page.locator("//p[contains(@class, 'Formstyle__ErrorMessage') and text()='This field is required']")).toBeVisible()
+            await page.locator("div.ck-editor__editable").fill(`${body_content} ${body}.`)
+            await page.locator("label:has-text('Body')").click()
+            await page.click("button.button span:has-text('Save')")
+            await page.locator("//a[text()='Close']").click()
+        })
+
+        // test that clicking the back button goes to the previous page
+        test('back button is working', async ({ page }) => {
+            const menu = await page.locator("//span[contains(@class, 'Stepperstyle__StepLabel-sc')]").allInnerTexts()
+            const current = await page.locator("span.eOSPvU").innerText()
+            const currentIndex = menu.indexOf(current)
+            console.log(`Current menu: ${current}, index: ${currentIndex}`)
+            await page.locator("button.button span:has-text('Back')").click()
+            const active = await page.locator("span.eOSPvU").innerText()
+            const activeIndex = menu.indexOf(active)
+            console.log(`Current menu: ${active}, index: ${activeIndex}`)
+            expect(activeIndex).toBeLessThan(currentIndex)
         })
     })
 })
--- tests/employer-hub/events.spec.js
+++ tests/employer-hub/events.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test')
 const { getRandomNumber, getRandomCharacters } = require("../../common/common-functions")
-const { CompleteLogin } = require("../../common/common-classes")
+const { CompleteLogin, Input } = require("../../common/common-classes")
 const data = require("../../common/common-details.json")
 
 test.beforeEach(async ({ page }) => {
@@ -35,5 +35,200 @@ test.describe('test for events on the employer hub', async () => {
         await page.locator("//div[contains(@class, 'Content__ContentBox-sc')]//a[text()='Edit']").nth(0).waitFor()
         const name = await page.locator("//h3[contains(@class, 'EventTeaserstyle__Title-sc')]").nth(0).innerText()
         expect(name).toEqual(`New Event - ${random}`)
+    })
+
+    // test to update the event organiser
+    // check that it was saved
+    test('update event organiser', async ({ page }) => {
+        const random = getRandomCharacters(6)
+        await page.locator("input[name=organiser]").fill(`Different Event Organiser - ${random}`)
+        await page.click("button.button span:has-text('Save')")
+        await page.locator("//button[text()='Close']").click()
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("//a[contains(@class, 'Navigationstyle__MenuLink') and span/text()='Events']").nth(1).click()
+        ])
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("//div[contains(@class, 'Content__ContentBox-sc')]//a[text()='Edit']").nth(0).click()
+        ])
+        const name = await page.locator("input[name=organiser]").getAttribute("value")
+        expect(name).toEqual(`Different Event Organiser - ${random}`)
+    })
+
+    // test to update the URL website
+    // check that it was saved
+    test('update URL website', async ({ page }) => {
+        await page.locator("input[name=url]").fill("invalid url")
+        await page.click("button.button span:has-text('Save')")
+        await expect(page.locator("//p[contains(@class, 'Formstyle__ErrorMessage') and text()='URL is not valid']")).toBeVisible()
+        const random = getRandomCharacters(6)
+        await page.locator("input[name=url]").fill(`${data.employerHubUrl}/${random}`)
+        await page.click("button.button span:has-text('Save')")
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("//a[text()='Preview']").click()
+        ])
+        const url = await page.locator("//a[span/text()='Website']").getAttribute("href")
+        expect(url).toEqual(`${data.employerHubUrl}/${random}`)
+    })
+
+    // test to update the time zone
+    // check that it was saved
+    test('update time zone', async ({ page }) => {
+        const label = page.locator("//span[label/text()='Time Zone']/following-sibling::div")
+        const getValue = await label.locator("input").getAttribute("value")
+        let newValue = "Sydney"
+        if (getValue == "Sydney") {
+            newValue = "Manila"
+        }
+        await label.locator("input").fill(newValue)
+        await label.locator(`li:has-text('${newValue}')`).click()
+        await page.click("button.button span:has-text('Save')")
+        await page.locator("//button[text()='Close']").click()
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("//a[contains(@class, 'Navigationstyle__MenuLink') and span/text()='Events']").nth(1).click()
+        ])
+        await page.locator("//div[contains(@class, 'Content__ContentBox-sc')]//a[text()='Edit']").nth(0).waitFor()
+        const eventDetails = await page.locator("//p[contains(@class, 'EventTeaserstyle__EventDetails-sc')]").nth(0).innerText()
+        expect(eventDetails).toContain(newValue)
+    })
+
+    // TODO: update the event start date and time
+    // check for error messages when the field was left blank
+    // check that it's also not possible to have finish date before start date
+    test("update event start and finish date and time", async ({ page }) => {
+
+    })
+
+    // TODO: update the event location
+    // check for error messages when the field was left blank
+    test("update event location", async ({ page }) => {
+
+    })
+
+    // update the event cost and check that it was saved
+    test('update event cost', async ({ page }) => {
+        const cost = getRandomNumber(10000, 100000)
+        console.log(`Cost: ${cost}`)
+        await page.locator("input[name=cost]").fill(cost.toString())
+        await page.click("button.button span:has-text('Save')")
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("//a[text()='Preview']").click()
+        ])
+        const costPreview = await page.locator("//div[contains(@class, 'field-label') and text()='Cost']/following-sibling::div[contains(@class, 'field-item')]").innerText()
+        expect(costPreview).toEqual(cost.toString())
+    })
+
+    // test for the industry sector section
+    test('update industry sector', async ({ page }) => {
+        await page.locator("//div[contains(@class, 'CheckboxTree__Container')]").nth(0).waitFor()
+        const label = page.locator("//span[label/text()='Industry Sector']/following-sibling::div")
+        const industrySectors = label.locator("//div[contains(@class, 'CheckboxGroup__CheckboxContainer-sc')]")
+        const countSectors = await industrySectors.count()
+        for (let i = 0; i < countSectors; i++) {
+            const isChecked = await industrySectors.nth(i).locator("input").getAttribute("class")
+            if (isChecked == "is-checked") {
+                await industrySectors.nth(i).locator("label").click()
+                const getClass = await industrySectors.nth(i).locator("input").getAttribute("class")
+                expect(getClass).not.toEqual("is-checked")
+            }
+        }
+        for (let i = 0; i < countSectors; i++) {
+            const random = getRandomNumber(1, 2)
+            if (random == 1) {
+                await industrySectors.nth(i).locator("label").click()
+                const getClass = await industrySectors.nth(i).locator("input").getAttribute("class")
+                expect(getClass).toEqual("is-checked")
+            }
+        }
+        await page.click("button.button span:has-text('Save')")
+        await page.locator("//button[text()='Close']").click()
+    })
+
+    // test for the study field section
+    test('update study field', async ({ page }) => {
+        await page.locator("//div[contains(@class, 'CheckboxTree__Container')]").nth(1).waitFor()
+        const label = page.locator("//span[label/text()='Study Field']/following-sibling::div")
+        const industrySectors = label.locator("//div[contains(@class, 'CheckboxGroup__CheckboxContainer-sc')]")
+        const countSectors = await industrySectors.count()
+        for (let i = 0; i < countSectors; i++) {
+            const isChecked = await industrySectors.nth(i).locator("input").getAttribute("class")
+            if (isChecked == "is-checked") {
+                await industrySectors.nth(i).locator("label").click()
+                const getClass = await industrySectors.nth(i).locator("input").getAttribute("class")
+                expect(getClass).not.toEqual("is-checked")
+            }
+        }
+        for (let i = 0; i < countSectors; i++) {
+            const random = getRandomNumber(1, 2)
+            if (random == 1) {
+                await industrySectors.nth(i).locator("label").click()
+                const getClass = await industrySectors.nth(i).locator("input").getAttribute("class")
+                expect(getClass).toEqual("is-checked")
+            }
+        }
+        await page.click("button.button span:has-text('Save')")
+        await page.locator("//button[text()='Close']").click()
+    })
+
+    // test to check that the region section is updating
+    test('update region', async ({ page }) => {
+        const input = new Input(page)
+        const label = page.locator("//span[label/text()='Region']/following-sibling::div")
+        await label.locator("//button[text()='Remove']").waitFor()
+        const remove = label.locator("//button[text()='Remove']")
+        const removeCount = await remove.count()
+        for (let i = 0; i < removeCount; i++) {
+            await remove.nth(0).click()
+        }
+        await label.locator("//button[text()='Add']").click()
+        await input.randomSelect("//span[label/text()='Region']/following-sibling::div//select[contains(@class, 'sc-khIgXV')]", false)
+        await page.click("button.button span:has-text('Save')")
+        await page.locator("//button[text()='Close']").click()
+    })
+
+    // test to update the event introduction
+    // check that it was saved and matching what shows on the list page and preview page
+    test('update event introduction', async ({ page }) => {
+        const intro = getRandomCharacters(6)
+        const intro_content = "Event introduction with some random characters:"
+        await page.locator("textarea[name=introduction]").fill(`${intro_content} ${intro}.`)
+        await page.click("button.button span:has-text('Save')")
+        await page.locator("//button[text()='Close']").click()
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("//a[contains(@class, 'Navigationstyle__MenuLink') and span/text()='Events']").nth(1).click()
+        ])
+        await page.locator("a:has-text('Edit')").nth(0).waitFor()
+        const introListPage = await page.locator("//p[contains(@class, 'EventTeaserstyle__Description-sc')]").nth(0).innerText()
+        expect(introListPage).toEqual(`${intro_content} ${intro}.`)
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("//a[span/text()='Preview']").nth(0).click()
+        ])
+        const introPreview = await page.locator("//p[contains(@class, 'PageHeadingstyle__Subheading')]").nth(0).innerText()
+        expect(introPreview).toEqual(`${intro_content} ${intro}.`)
+    })
+
+    // test to update the event description
+    // check that it was saved and matching what shows on the preview page
+    test('update event description', async ({ page }) => {
+        const label = page.locator("//span[label/text()='Description']/following-sibling::div")
+        await label.locator("div.ck-editor__editable").click()
+        await page.keyboard.press("Control+A")
+        await page.keyboard.press("Delete")
+        const description = getRandomCharacters(6)
+        const description_content = "Event description with some random characters:"
+        await label.locator("div.ck-editor__editable").fill(`${description_content} ${description}.`)
+        await page.click("button.button span:has-text('Save')")
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("//a[text()='Preview']").click()
+        ])
+        const descriptionPreview = await page.locator("//div[contains(@class, 'ViewEventstyle__OverviewContainer-sc')]/p").innerText()
+        expect(descriptionPreview).toEqual(`${description_content} ${description}.`)
     })
 })
