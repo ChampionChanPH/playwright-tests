0227eacc5844caf0ef68bbde80b9aee96ce6e903
Chan <christian.anasco@gmail.com>
2022-06-28 18:03:46 +0800

    added complete ve process

--- tests/student-hub/single-page.spec.js
+++ tests/student-hub/single-page.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test')
 const { getRandomNumber, getRandomCharacters } = require('../../common/common-functions')
-const { CompleteLogin } = require("../../common/common-classes")
+const { CompleteLogin, VirtualExperience } = require("../../common/common-classes")
 const data = require("../../common/common-details.json")
 
 const csv = "./resources/blank.xlsx"
@@ -39,5 +39,67 @@ test.describe('single page tests', async () => {
         await page.goto(`${url}/search-jobs`)
         await page.waitForSelector("div.viewport--normal a.logo")
         expect(page.url()).toContain("?locations=")
+    })
+
+    test.only('user complete ve', async ({ page }) => {
+        test.skip(data.studentHubUrl == "https://gradaustralia.com.au", "skip if it's a live testing")
+        const virtualExperience = new VirtualExperience()
+        await virtualExperience.deleteRegistration()
+        await page.goto(`${data.studentHubUrl}/graduate-employers/prosple/jobs-internships/prosple-summer-internship-program`)
+        const login = new CompleteLogin(page)
+        await login.studentHubLogin()
+        await page.locator("a[data-event-track=cta-ve-register]").first().click()
+        await page.locator("input#organisation-consent").click()
+        await page.locator("button:has-text('Submit')").click()
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("a:has-text('Continue to experience.')").click()
+        ])
+        await page.locator("div[data-testid=virtual-experience-status-badge] p").waitFor()
+        await page.locator("div[data-testid=virtual-experience-status-badge] p:has-text('FETCHING STATUS...')").waitFor({ state: 'hidden' })
+        let status = await page.locator("div[data-testid=virtual-experience-status-badge] p").innerText()
+        expect(status).toEqual("NOT STARTED")
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("a:has-text('Go to Module')").first().click()
+        ])
+        await page.locator("button:has-text('Submit')").click()
+        await expect(page.locator("//div[contains(@class, 'webform__element--error') and text()='Field is required.']")).toBeVisible()
+        await page.locator("input#what_is_your_first_name_").fill("Prosple")
+        await page.locator("button:has-text('Submit')").click()
+        await page.locator("li:has-text('Module was submitted successfully. ')").waitFor()
+        await page.locator("div[data-testid=module-status-badge] p").waitFor()
+        await page.locator("div[data-testid=module-status-badge] p:has-text('FETCHING STATUS...')").waitFor({ state: 'hidden' })
+        status = await page.locator("div[data-testid=module-status-badge] p").innerText()
+        expect(status).toEqual("COMPLETED")
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("a:has-text('Overview')").click()
+        ])
+        await page.locator("div[data-testid=virtual-experience-status-badge] p").waitFor()
+        await page.locator("div[data-testid=virtual-experience-status-badge] p:has-text('FETCHING STATUS...')").waitFor({ state: 'hidden' })
+        status = await page.locator("div[data-testid=virtual-experience-status-badge] p").innerText()
+        expect(status).toEqual("IN PROGRESS")
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("a:has-text('Go to Module')").last().click()
+        ])
+        await page.locator("button:has-text('Submit')").click()
+        await expect(page.locator("//div[contains(@class, 'webform__element--error') and text()='Field is required.']")).toBeVisible()
+        await page.locator("input#what_is_your_last_name_").fill("Prosple")
+        await page.locator("button:has-text('Submit')").click()
+        await page.locator("li:has-text('Module was submitted successfully. ')").waitFor()
+        await page.locator("div[data-testid=module-status-badge] p").waitFor()
+        await page.locator("div[data-testid=module-status-badge] p:has-text('FETCHING STATUS...')").waitFor({ state: 'hidden' })
+        status = await page.locator("div[data-testid=module-status-badge] p").innerText()
+        expect(status).toEqual("COMPLETED")
+        await Promise.all([
+            page.waitForNavigation(),
+            page.locator("a:has-text('Overview')").click()
+        ])
+        await page.locator("div[data-testid=virtual-experience-status-badge] p").waitFor()
+        await page.locator("div[data-testid=virtual-experience-status-badge] p:has-text('FETCHING STATUS...')").waitFor({ state: 'hidden' })
+        status = await page.locator("div[data-testid=virtual-experience-status-badge] p").innerText()
+        expect(status).toEqual("COMPLETED")
     })
 })
